03f2d7e62a07372ada7649d8caea13d87eefdfef
murilofsouzaa <onemurilo@gmail.com>
2026-08-23 12:29:13 -0300

    feat: adding 25:00 default timer

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -4,39 +4,57 @@ const Homepage = () => {
 
     const [minutes, setMinutes] = useState<number>(25);
     const [seconds, setSeconds] = useState<number>(59);
-    const [isActive, setIsActive] = useState<boolean>(true)
+    const [isFirstTime, setIsFirstTime] = useState<boolean>(true);
+    const [isActive, setIsActive] = useState<boolean>(false);
 
     useEffect(() => {
-        const timerId = setInterval(() =>{
-            setSeconds((prev):number => {
-                if(prev > 0){
-                  return prev - 1;  
-                }else{
-                    setMinutes((prev):number => {
-                        if(prev > 0){
-                            setSeconds(59);
-                            return prev - 1;
-                        }else{
-                            return 0;
-                        }
-                    })
-                    setIsActive((prev) => !prev)
-                    return 0;
-                }
-            } 
-        )
-        }, 10)
-        return () => clearInterval(timerId);
-    }, []);
+        if(isActive) {
+            const timerId = setInterval(() =>{
+                setSeconds((prev):number => {
+                    if(prev > 0){
+                      return prev - 1;  
+                    }else{
+                        setMinutes((prev):number => {
+                            if(prev > 0){
+                                setSeconds(59);
+                                return prev - 1;
+                            }else{
+                                return 0;
+                            }
+                        })
+                        setIsActive((prev) => !prev)
+                        return 0;
+                    }
+                } 
+            )
+            }, 1000)
+            return () => clearInterval(timerId);
+        }
+    }, [isActive]);
 
     return ( 
         <div className="flex justify-center items-center bg-(--bg-color) h-screen">
-            <div className="timer-subcontainer flex justify-center items-center bg-(--circle-color) border-10 border-(--timer-stroke) h-150 w-150 rounded-[100%]">
-                <div className="text-white text-8xl font-bold">
-                    {!isActive ? <span>25:00</span> : <span>{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</span> }
+            <button
+                type="button"
+                onClick={() => {
+                    setIsFirstTime(false);
+                    setIsActive((prev) => !prev)
+                }}
+                className="timer-subcontainer flex flex-col justify-center items-center bg-(--circle-color) border-10 border-(--timer-stroke) 
+                h-150 w-150 rounded-[100%] hover:scale-[0.98] hover:cursor-pointer transition-all">
+                <div className="text-white text-9xl font-bold">
+                    {isFirstTime ? 
+                        <span>25:00</span>
+                    :
+                        <span>{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</span>
+                    }
                 </div>
-            </div>
-            <button type="button"></button>
+            <button 
+                type="button"
+                className="font-semibold text-2xl text-white py-2 px-8 cursor-pointer"
+                >{!isActive? "START" : "PAUSE"}
+            </button>
+            </button>
         </div>
      );
 }
