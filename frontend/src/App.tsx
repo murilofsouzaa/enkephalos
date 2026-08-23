ed0ffab2c120002ecdf9e43e51058d7fb7c25f74
Murilo <onemurilo@gmail.com>
2026-08-23 10:29:52 -0300

    folders strucuture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Homepage from './components/Homepage/Homepage'
+import Header from './components/Header/Header'
 
 
 function App() {
 
   return (
     <>
-     
+     <Header></Header>
+     <Homepage></Homepage>
     </>
   )
 }
