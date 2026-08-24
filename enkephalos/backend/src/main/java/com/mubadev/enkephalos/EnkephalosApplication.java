a45bfe678a23b296fd2fa0ad106f4f37d821ab5c
murilofsouzaa <onemurilo@gmail.com>
2026-08-24 11:42:35 -0300

    feat(backend): adding springboot initial strucuture

---
+++ enkephalos/backend/src/main/java/com/mubadev/enkephalos/EnkephalosApplication.java
@@ -0,0 +1,13 @@
+package com.mubadev.enkephalos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnkephalosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnkephalosApplication.class, args);
+	}
+
+}
---
+++ enkephalos/backend/src/test/java/com/mubadev/enkephalos/EnkephalosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mubadev.enkephalos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnkephalosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
